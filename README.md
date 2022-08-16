31b8c1b523668d7f76fcc2ae404ac94aca80d16b
trick <riccaruf@hotmail.com>
2022-08-17 01:36:49 +0200

    first component completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+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
+
 # LocalWeatherApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
 
 @Component({
   selector: 'app-current-weather',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentWeatherComponent implements OnInit {
 
-  constructor() { }
+  current:ICurrentWeather
+
+  constructor() { 
+    
+    this.current = {
+      city:"Modena",
+      country:"Italy",
+      date: new Date(),
+      description: "Sunny",
+      image: "assets/img/sunny.png",
+      temperature: 20
+
+    }as ICurrentWeather
+  }
 
   ngOnInit(): void {
   }
